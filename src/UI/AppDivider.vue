4291bcb32003a43391c6f9df18a15d83c627c60c
Ilya Garenko <il.gareni4@gmail.com>
2023-06-22 17:53:10 +0300

    Added Divider

---
+++ src/UI/AppDivider.vue
@@ -0,0 +1,11 @@
+<template>
+     <div class="divider" />
+</template>
+
+<style scoped lang="scss">
+.divider {
+    height: 1px;
+    width: 100%;
+    background-color: var(--c-divider);
+}
+</style>
