77abe7208de607b50c29bb49e002ad01e19109f0
Ilya <il.gareni4@gmail.com>
2023-06-23 23:59:12 +0300

    Imported MathJax through index.html

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,8 @@ import router from './router'
 import VueMathfield from "https://unpkg.com/mathlive/dist/vue-mathlive.mjs";
 import * as MathLive from 'mathlive'
 
-import MathJax, { initMathJax, renderByMathjax } from "mathjax-vue3";
 const app = createApp(App)
-function onMathJaxReady() {
 
-  const el = document.getElementById("elementId");
-  renderByMathjax(el);
-}
-initMathJax({}, onMathJaxReady);
-
-app.use(MathJax)
 app.use(VueMathfield, MathLive);
 // app.component("custom-tag", Mathfield);
 app.use(router)
